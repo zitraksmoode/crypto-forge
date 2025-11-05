b6d521a03875c76f2277ead9bb6c8091086506ed
zitraksmoode <Cybindmitrij711@gmail.com>
2025-11-05 20:25:02 +0500

    Initial: Spring Boot project setup

---
+++ src/main/java/com/zitraksmoode/crypto/forge/CryptoForgeApplication.java
@@ -0,0 +1,13 @@
+package com.zitraksmoode.crypto.forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoForgeApplication.class, args);
+	}
+
+}
